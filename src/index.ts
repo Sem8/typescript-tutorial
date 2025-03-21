7aaa8f3545b1e436385ef7aa9009100f857a4ff0
Sem8 <mssemmi8@gmail.com>
2025-03-21 18:29:58 -0400

    - TS for type aliases and union types

--- src/index.ts
+++ src/index.ts
@@ -6,15 +6,15 @@
 
 // Built-in types
 
-let sales: number = 123_456_789
-let course: string = 'Typescript';
+let sales: number = 123_456_789;
+let course: string = "Typescript";
 let is_published: boolean = true;
 let level;
 
 /* 1. any Type */
 
 level = 1;
-level = 'a';
+level = "a";
 
 // function render(document) {
 //     console.log(document)
@@ -22,7 +22,6 @@ level = 'a';
 // function render(document: any) {
 //     console.log(document)
 // }
-
 
 /* 2. Arrays */
 
@@ -38,22 +37,24 @@ level = 'a';
 // // get access to all number methods when declare array elements as numbers
 // numbers.forEach(n => n.)
 
-
 /* 3. Tuples */
 
-let user: [number, string] = [1, 'Mosh']
+let user: [number, string] = [1, "Mosh"];
 // user[0].toExponential   // automatically get number methods b/c defined as number
 // user[1].charAt   // automatically get string methods b/c defined as string
-
 
 /* 4. enums */
 const small = 1;
 const medium = 2;
 const large = 3;
 
-// PascalCase 
-const enum Size {Small = 1, Medium, Large}
-let mySize = Size.Medium
+// PascalCase
+const enum Size {
+  Small = 1,
+  Medium,
+  Large,
+}
+let mySize = Size.Medium;
 console.log(mySize);
 
 /* 5. Functions */
@@ -70,32 +71,80 @@ console.log(mySize);
 
 // // Make taxYear parameters optional with a ?
 // function calculateTax(income: number, taxYear?: number): number {
-//     if (taxYear < 2022) 
+//     if (taxYear < 2022)
 //         return income * 1.2;
 //     return income * 1.3
 // }
 // calculateTax(10_000) // - won't give error even though we didn't provide taxYear parameter
 
-
 // final function
 function calculateTax(income: number, taxYear = 2022): number {
-    // let x; //  noUnusedLocals will complain about this
-    if (taxYear < 2022) 
-        return income * 1.2;
-    return income * 1.3
+  // let x; //  noUnusedLocals will complain about this
+  if (taxYear < 2022) return income * 1.2;
+  return income * 1.3;
 }
 
-calculateTax(10_000)
-
+calculateTax(10_000);
 
 /* 6. Objects */
 
+// // 6a.
 // let employee = {id: 1 };
 // employee.name = 'Mosh '  // error bc typescript already inferred shape of object above
 
-let employee: {
-    id: number,
-    name: string
-} = {id: 1 };
+// // 6b. Make optional property
+// let employee: {
+//     id: number,
+//     name?: string
+// } = {id: 1 };
 
-employee.name = 'Mosh'
+// employee.name = 'Mosh'
+
+// // 6c. Correct way is to initialize with empty string if no name yet
+// let employee: {
+//     id: number,
+//     name: string
+// } = {id: 1, name: ''};
+
+// employee.name = 'Mosh'
+
+// // 6d. Make some properties read only but cannot change
+// let employee: {
+//   readonly id: number;
+//   name: string;
+//   retire: (date: Date) => void; // function structure inside the object
+// } = { id: 1, name: "Sem", retire: (date: Date) => {
+//     console.log(date)
+// }};
+
+// employee.id = 0;  // error bc id is read only
+
+/* 7. Type aliases */
+
+type Employee = {
+  readonly id: number;
+  name: string;
+  retire: (date: Date) => void;
+};
+
+let employee: Employee = {
+  id: 1,
+  name: "Sem",
+  retire: (date: Date) => {
+    console.log(date);
+  },
+};
+
+/* 3. Union Types - Give a function parameter more than one type */
+function kgToLbs(weight: number | string): number {
+    // Narrowing
+    if (typeof weight === 'number') {
+        // weight. // TS knows to give us number methods only
+        return weight * 2.2;
+    } else {
+        return parseInt(weight) * 2.2;
+    }
+}
+
+kgToLbs(10);
+kgToLbs('10kg');
