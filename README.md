1be7019da0906bd74614a0d74a44cc7383d0800a
Sem8 <mssemmi8@gmail.com>
2025-03-24 15:43:33 -0400

    - Optional call

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ Typescript tutorials and info
 
 - To run JS version of the code after compiling
 `node dist/index.js`
+
+- To compile and run code at same time 
+```
+tsc && node dist/index.js
+```
--- src/index.ts
+++ src/index.ts
@@ -135,7 +135,7 @@ let employee: Employee = {
   },
 };
 
-/* 3. Union Types - Give a function parameter more than one type */
+/* 8. Union Types - Give a function parameter more than one type */
 function kgToLbs(weight: number | string): number {
     // Narrowing
     if (typeof weight === 'number') {
@@ -148,3 +148,91 @@ function kgToLbs(weight: number | string): number {
 
 kgToLbs(10);
 kgToLbs('10kg');
+
+/* 9. Intersection types - something that can be one type as well as the other */
+
+type Draggable = {
+    drag: () => void
+}
+
+type Resizable = {
+    resize: () => void
+}
+
+type UIWidget = Draggable & Resizable;
+
+let textBox: UIWidget = {
+    drag: () => {},
+    resize: () => {}
+}
+
+
+/* 10. Literal types -  limit values we can assign to a variable */
+
+// let quantity: number;  // problem is it can take any number not just bt 50 or 100 which is our desired exact nums
+
+// Literal (exact, specific)
+// let quantity: 50 = 50;  // CORRECT
+// let quantity1: 50 = 51;  // INCORRECT
+
+// 10a. With Union operator
+// // Literal (exact, specific)
+// let quantity: 50 | 100 = 100;
+
+// Literal (exact, specific)
+type Quantity = 50 | 100;
+let quantity: Quantity = 100
+
+
+/* 11. Nullable types */
+// // 11a.
+// function greet(name: string) {
+//     if (name) {
+//         console.log(name.toUpperCase());
+//     } else {
+//         console.log('Hola');
+//     }
+// }
+// greet(null); // Error bc can't use toUpperCase method on null; setting strictNullChecks: false will not complain about this
+
+// 11b.
+function greet(name: string | null | undefined) {
+    if (name) {
+        console.log(name.toUpperCase());
+    } else {
+        console.log('Hola');
+    }
+}
+
+greet(null); // now acceptable
+greet(undefined); // now acceptable
+
+
+/* 12. Optional Chaining */
+type Customer = {
+    birthday?: Date
+};
+
+function getCustomer(id: number): Customer | null | undefined {
+    return id === 0 ? null : {birthday: new Date()}
+}
+
+let customer = getCustomer(0)
+
+// // 12a. traditional way to handle null or undefined value for object property
+// if (customer !== null && customer !== undefined) {
+//     console.log(customer.birthday)
+// }
+
+// 12b. Optional property access operator
+console.log(customer?.birthday?.getFullYear())  // result is undefined
+
+// // 12c. Optional element access operator for arrays
+// customer?.[0]
+
+// 12d. Optional call
+// let log: any = (message: string) => console.log(message)
+
+let log: any = null;
+// log('a')  // w/ log set to null calling this function will crash since set to null
+// log?.('a')  // correct way to handle so program doesn't crash
